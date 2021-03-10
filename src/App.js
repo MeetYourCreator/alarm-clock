4b490990510ddfd714e85ba09f68a799eb132f5e
MeetYourCreator <ramizackshamir@icloud.com>
2021-03-10 16:59:37 -0500

    init app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Home from './screens/Home/Home.jsx'
+import Clock from './screens/Clock/Clock.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Clock />
+   </>
+  );
+}
+
+export default App;
