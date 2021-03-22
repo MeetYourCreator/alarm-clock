cd90b08e63b25ad7facd33d97dccfc1e1a28fc28
MeetYourCreator <ramizackshamir@icloud.com>
2021-03-22 16:03:22 -0400

    refactor Clock.css

--- src/services/time.js
+++ src/services/time.js
@@ -1,7 +1,3 @@
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
-
 export const setHour = () => {
   const date = new Date();
   const secondRatio = date.getSeconds() / 60;
@@ -9,9 +5,6 @@ export const setHour = () => {
   const hourRatio = (minuteRatio + date.getHours()) / 12;
 
   return hourRatio
-  // setRotation(secondHand, secondsRatio)
-  // setRotation(secondHand, minutesRatio)
-  // setRoation(secondHand, hoursRatio)
 }
 
 export const setMinute = () => {
@@ -20,9 +13,6 @@ export const setMinute = () => {
   const minuteRatio = (secondRatio + date.getMinutes()) / 60;
   
   return minuteRatio
-  // setRotation(secondHand, secondsRatio)
-  // setRotation(secondHand, minutesRatio)
-  // setRoation(secondHand, hoursRatio)
 }
 
 export const setSecond = () => {
@@ -39,6 +29,3 @@ export const angleClock = (hour, minutes) => {
   const angle = Math.abs(hourAngle - minuteAngle)
   return Math.min(angle, 360 - angle)
 }
-// setRotation = (element, rotationRatio) => {
-//   element.style.setProperty('--rotation', rotationRatio * 360)
-// }
