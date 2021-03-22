352d34847905301fda03c42280b3de5b1fe8d506
MeetYourCreator <ramizackshamir@icloud.com>
2021-03-22 09:15:27 -0400

    refactor components for styled init

--- src/screens/Clock/Clock.jsx
+++ src/screens/Clock/Clock.jsx
@@ -8,6 +8,7 @@ import { setSeconds } from "../../services/time.js"
 
 import { ClockContainer } from "../../Styled/ClockContainer.js"
 import { ClockFace } from '../../Styled/ClockFace.js'
+import { ClockStyled } from '../../Styled/ClockStyled.js'
 
 
 
@@ -16,26 +17,27 @@ const Clock = () => {
   return (
     <>
       <ClockContainer>
-        {/* <div className="clock"> */}
+        <div className="clock">
+        {/* <ClockStyled> */}
           <ClockFace>
-            <HourHand name='Hours'/>
-            <MinuteHand name='Minutes'/>
-            <SecondHand name='Seconds'/>
-              <div className="number number1">1</div>
-              <div className="number number2">2</div>
-              <div className="number number3">3</div>
-              <div className="number number4">4</div>
-              <div className="number number5">5</div>
-              <div className="number number6">6</div>
-              <div className="number number7">7</div>
-              <div className="number number8">8</div>
-              <div className="number number9">9</div>
-              <div className="number number10">10</div>
-              <div className="number number11">11</div>
-              <div className="number number12">12</div>
-              
+            <HourHand name="Hours" />
+            <MinuteHand name="Minutes" />
+            <SecondHand name="Seconds" />
+            <div className="number number1">1</div>
+            <div className="number number2">2</div>
+            <div className="number number3">3</div>
+            <div className="number number4">4</div>
+            <div className="number number5">5</div>
+            <div className="number number6">6</div>
+            <div className="number number7">7</div>
+            <div className="number number8">8</div>
+            <div className="number number9">9</div>
+            <div className="number number10">10</div>
+            <div className="number number11">11</div>
+            <div className="number number12">12</div>
           </ClockFace>
-        {/* </div> */}
+        {/* </ClockStyled> */}
+        </div>
       </ClockContainer>
     </>
   )
