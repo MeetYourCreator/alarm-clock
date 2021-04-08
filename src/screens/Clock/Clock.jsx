cfb995f5cc8fe70fe4b7a40630b2f9e87afafdac
MeetYourCreator <ramizackshamir@icloud.com>
2021-04-08 09:45:12 -0400

    clean up useEffect

--- src/screens/Clock/Clock.jsx
+++ src/screens/Clock/Clock.jsx
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react"
 import ClockNumber from "../../Components/ClockNumber.jsx"
 import SecondHand from "../../Components/SecondHand.jsx"
 import MinuteHand from "../../Components/MinuteHand.jsx"
 import HourHand from "../../Components/HourHand.jsx"
-import {setHour, setMinute, setSecond } from '../../services/time.js'
-import './Clock.css'
+import { setHour, setMinute, setSecond } from "../../services/time.js"
+import "./Clock.css"
 
 const Clock = () => {
-  const [hourRatio, setHourRatio] = useState(0)
-  const [minuteRatio, setMinuteRatio] = useState(0)
-  const [secondRatio, setSecondRatio] = useState(0)
-  
+  const [hourRatio, setHourRatio] = useState(setHour())
+  const [minuteRatio, setMinuteRatio] = useState(setMinute())
+  const [secondRatio, setSecondRatio] = useState(setSecond())
+
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setHourRatio(setHour())
       setMinuteRatio(setMinute())
       setSecondRatio(setSecond())
-  }, 1000)
-  }, [])
+    }, 1000)
 
-  
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <>
@@ -50,48 +50,23 @@ const Clock = () => {
             >
               {/* <SecondHand /> */}
             </div>
-            <div className="number number1">
-              1
-            </div>
-            <div className="number number2">
-              2
-            </div>
-            <div className="number number3">
-              3
-            </div>
-            <div className="number number4">
-              4
-            </div>
-            <div className="number number5">
-              5
-            </div>
-            <div className="number number6">
-              6
-            </div>
-            <div className="number number7">
-              7
-            </div>
-            <div className="number number8">
-              8
-            </div>
-            <div className="number number9">
-              9
-            </div>
-            <div className="number number10">
-              10
-            </div>
-            <div className="number number11">
-              11
-            </div>
-            <div className="number number12">
-              12
-            </div>
+            <div className="number number1">1</div>
+            <div className="number number2">2</div>
+            <div className="number number3">3</div>
+            <div className="number number4">4</div>
+            <div className="number number5">5</div>
+            <div className="number number6">6</div>
+            <div className="number number7">7</div>
+            <div className="number number8">8</div>
+            <div className="number number9">9</div>
+            <div className="number number10">10</div>
+            <div className="number number11">11</div>
+            <div className="number number12">12</div>
           </div>
         </div>
       </div>
     </>
   )
-
 }
 
-export default Clock;
+export default Clock
